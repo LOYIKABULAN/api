bddb3f5d5ba087c17963ad9f87d5965bc1a1f2e4
guankaihua <876224752@qq.com>
2022-02-07 10:58:21 +0800

    解决数据库时区

--- src/db/seq.js
+++ src/db/seq.js
@@ -10,7 +10,8 @@ const {
 } = require("../config/config.default");
 const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD, {
   host: MYSQL_HOST,
-  dialect: "mysql" /* 选择 'mysql' | 'mariadb' | 'postgres' | 'mssql' 其一 */,
+  dialect: "mysql", /* 选择 'mysql' | 'mariadb' | 'postgres' | 'mssql' 其一 */
+  timezone: "+08:00",
 });
 //用来测试是否成功连接数据库
 seq
